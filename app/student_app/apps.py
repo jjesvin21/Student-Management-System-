ecb9de04ee9ab93af4667654da2ac0ba5946b825
jjesvin21 <jjesvin21jose@gmail.com>
2023-05-18 12:01:17 +0530

    Changed Structure

--- app/student_app/apps.py
+++ app/student_app/apps.py
@@ -1,6 +1,7 @@
 from django.apps import AppConfig
 
 
-class AppStudentManagementConfig(AppConfig):
+class StudentAppConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "app_student_management"
+    name = "app.student_app"
+
